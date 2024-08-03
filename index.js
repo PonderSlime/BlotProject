f8f8fc4aad2e1b1f32721be5724e924a9d4b4087
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-02 22:29:05 -0600

    Circle randomization perfected

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 @author: PonderSlime
 @snapshot: the name of the snapshot file you want in the gallery*/
 
-const width = 125;
-const height = 125;
+const canvasWidth = 125;
+const canvasHeight = 125;
 
 const lineThickness = 1.5;
 const line_spacing = 1.5;
-const num1 = bt.randInRange(0, 180);
-const num2 = bt.randInRange(0, 2);
+const num1 = bt.randInRange(30, 180);
+const num2 = bt.randInRange(30, 180);
+const scale = (bt.randInRange(2, 11) + 0.7);
+const lineLength = 2.5 * scale
 
-setDocDimensions(width, height);
+setDocDimensions(canvasWidth, canvasHeight);
 
 const t = new bt.Turtle();
 
@@ -21,37 +23,22 @@ const createShape = (turtle, n, size) => {
   const turnAngle = 360 / n;  for (let i = 0; i < n; i++) {
     turtle.forward(size);
     turtle.left(turnAngle);  }};
-const createLines = (width, height) => {
+const createLines = (canvasWidth, canvasHeight) => {
   const t = new bt.Turtle();
-  for (let i = 0; i < height; i++) {
-    
-    t.forward(width);
+  for (let i = 0; i < canvasHeight; i++) {
+    t.forward(lineLength);
     t.up();
     t.left(num1);
-    t.forward(2);
+    t.forward(lineLength);
     t.left(num2);
-    t.forward(width);
-    t.left(num1 / num2 * num1);
+    t.forward(lineLength);
+    t.left(num1);
     t.down();
   }  
   return t.lines();
 }
-const lines = createLines(width/ 2, height/ 2);
-bt.translate(lines, [width / 2, height / 2], bt.bounds(lines).cc);
-bt.join(finalLines, lines)  // all these are off by 5, not really sure why
-bt.translate(finalLines, [5, 5]);
-/*const border = [
-  [[0, 0],
-  [width, 0]  ],
-  [[width, 0],
-  [width, height]  ],
-  [[width, height],
-  [0, height]  ],
-  [[0, height], 
-  [0, 0]  ],];
-bt.join(finalLines, border);*/
+const lines = createLines(canvasWidth, canvasHeight);
+bt.translate(lines, [canvasWidth / 2, canvasHeight / 2], bt.bounds(lines).cc);
+bt.join(finalLines, lines)
 
-//finalLines = finalLines.concat();
-//drawLines(line, { width: lineThickness });
-// draw it
 drawLines(finalLines);
